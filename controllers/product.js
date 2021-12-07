3cd30ba375bac2334744c2c2e07659a45aa77fa0
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-12-07 18:54:19 +0530

    set product update

--- controllers/product.js
+++ controllers/product.js
@@ -43,11 +43,11 @@ exports.create = (req, res) => {
     const product = new Product(fields);
 
     if (files.photo) {
-      if (files.photo.size < 2000000) {
-        return res.status(400).json({
-          error: "image should be less than 2mb in size",
-        });
-      }
+      // if (files.photo.size < 20000000) {
+      //   return res.status(400).json({
+      //     error: "image should be less than 2mb in size",
+      //   });
+      // }
 
       product.photo.data = fs.readFileSync(files.photo.filepath);
       product.photo.contentType = files.photo.type;
@@ -76,6 +76,49 @@ exports.remove = (req, res) => {
 
     res.json({
       message: "product deleted Successfully",
+    });
+  });
+};
+
+exports.update = (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.keepExtension = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploded",
+      });
+    }
+
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (!name | !description | !price | !category | !quantity | !shipping) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
+
+    let product = req.product;
+    product = _.extend(product, fields);
+
+    if (files.photo) {
+      // if (files.photo.size < 20000000) {
+      //   return res.status(400).json({
+      //     error: "image should be less than 2mb in size",
+      //   });
+      // }
+
+      product.photo.data = fs.readFileSync(files.photo.filepath);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(result);
     });
   });
 };
