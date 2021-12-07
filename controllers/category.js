9240edaa7a903346c3b91145c1db29872d221369
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-12-07 20:05:52 +0530

    category get by id

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,23 @@
 const Category = require("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.categoryId = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: "Category not found",
+      });
+    }
+    req.category = category;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  const category = req.category;
+  return res.json(category);
+};
+
 exports.create = (req, res) => {
   const category = new Category(req.body);
   category.save((err, data) => {
--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category");
+const { create, categoryId, read } = require("../controllers/category");
 const { userById } = require("../controllers/user");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
+router.get("/category/:categoryId", read);
 router.post("/category/create/:userId", requireSignin, isAdmin, create);
 
 router.param("userId", userById);
+router.param("categoryId", categoryId);
 
 module.exports = router;
