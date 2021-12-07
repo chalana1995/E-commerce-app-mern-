5cf384f0756bfde5ae1986bbb5a5f6b3985f5865
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-12-07 18:21:50 +0530

    create product by id

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/product");
+const { create, productById, read } = require("../controllers/product");
 const { userById } = require("../controllers/user");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
+router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAdmin, create);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
