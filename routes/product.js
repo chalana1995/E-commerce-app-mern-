057548c5158e7b8871c43d0126da59c9fb6861f0
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-12-08 20:26:00 +0530

    get all product with query params

--- routes/product.js
+++ routes/product.js
@@ -7,6 +7,7 @@ const {
   read,
   remove,
   update,
+  list,
 } = require("../controllers/product");
 const { userById } = require("../controllers/user");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
@@ -15,6 +16,7 @@ router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAdmin, create);
 router.delete("/product/:productId/:userId", requireSignin, isAdmin, remove);
 router.put("/product/:productId/:userId", requireSignin, isAdmin, update);
+router.get("/products", list);
 
 router.param("userId", userById);
 router.param("productId", productById);
