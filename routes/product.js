96be3bdaa4b8775fb68341713f09e43b788c1dc9
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-12-08 22:18:53 +0530

    get a photo

--- routes/product.js
+++ routes/product.js
@@ -11,6 +11,7 @@ const {
   listRelated,
   listCategories,
   listBySearch,
+  photo,
 } = require("../controllers/product");
 const { userById } = require("../controllers/user");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
@@ -23,6 +24,7 @@ router.get("/products", list);
 router.get("/products/categories", listCategories);
 router.get("/products/related/:productId", listRelated);
 router.post("/products/by/search", listBySearch);
+router.get("/products/photo/:productId", photo);
 
 router.param("userId", userById);
 router.param("productId", productById);
